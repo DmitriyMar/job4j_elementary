248bb6f3bfa1ccf6f00789b4cfeeb2fdf3559332
DmitriyMar <dmitriy-maraev@yandex.ru>
2021-09-30 11:03:32 +0300

    9.1. String [#237407 #210434]

--- src/test/java/ru/job4j/condition/SqArea.java
+++ src/test/java/ru/job4j/condition/SqArea.java
@@ -9,9 +9,9 @@ public class SqArea {
     public static void main(String[] args) {
         double result1 = SqArea.square(6, 2);
         System.out.println(" p = 6, k = 2, s = 1, real = " + result1);
-        double result2 = result1 * 2; // длина стороны l=h*k
+        double result2 = result1 * 2;
         System.out.println(" dlina storoni " + result2);
-        double result3 = result1 * result2; // S = l*k площадь
+        double result3 = result1 * result2;
         System.out.println("S = " + result3);
     }
 
