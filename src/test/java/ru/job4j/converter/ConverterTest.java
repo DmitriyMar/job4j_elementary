778e9c3588caa407fd7789ca1b8905fad6cbdd47
DmitriyMar <dmitriy-maraev@yandex.ru>
2021-10-07 07:58:03 +0300

    1. Модульные тесты. [#159435 #212706]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -3,7 +3,7 @@ package ru.job4j.converter;
 import org.junit.Test;
 import org.junit.Assert;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 public class ConverterTest {
 
