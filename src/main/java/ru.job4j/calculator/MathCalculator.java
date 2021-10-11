b28f05e51aad61c3549910827460de8b0b2d6061
DmitriyMar <dmitriy-maraev@yandex.ru>
2021-10-11 07:59:31 +0300

    Модульные тесты. [#159435 #212706]

--- src/main/java/ru.job4j/calculator/MathCalculator.java
+++ src/main/java/ru.job4j/calculator/MathCalculator.java
@@ -5,7 +5,7 @@ import static ru.job4j.math.MathFunction.*;
 public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
+         return sum(first, second)
                 + multiply(first, second);
     }
 
@@ -22,9 +22,9 @@ public class MathCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
-        System.out.println("Сумма разности и деления: " + minusAndDev(10,20));
-        System.out.println("Сумма всех чисел " + totalAm(10,20));
+        System.out.println("Результат расчета равен:" + sumAndMultiply(10, 20));
+        System.out.println("Сумма разности и деления:" + minusAndDev(10, 20));
+        System.out.println("Сумма всех чисел" + totalAm(10, 20));
     }
 }
 
