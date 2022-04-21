eca0a52ff22bfa6b7f0dc3495f38e18badbd33e5
DmitriyMar <dmitriy-maraev@yandex.ru>
2022-04-21 07:44:59 +0300

    5.7. Ипотека [#156315 #290273]

--- src/main/java/ru.job4j/loop/Mortgage.java
+++ src/main/java/ru.job4j/loop/Mortgage.java
@@ -5,7 +5,7 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         percent = percent / 100;
-        while (amount >= 0) {
+        while (amount > 0) {
             amount = amount * percent + amount;
             amount = amount - salary;
             year = year + 1;
