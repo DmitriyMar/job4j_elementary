d5ce62300f93911eaf028425e105a75e40724898
DmitriyMar <dmitriy-maraev@yandex.ru>
2022-04-25 13:44:05 +0300

    6.0.1. Объявление массива. [#3613 #293240]

--- src/main/java/ru.job4j/array/ArrayDefinition.java
+++ src/main/java/ru.job4j/array/ArrayDefinition.java
@@ -5,18 +5,6 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] price = new float[40];
-        String[] names = new String[4];
-        names[0] = "Petr";
-        names[1] = "Dmitriy";
-        names[2] = "Vladimir";
-        names[3] = "Oleg";
-        System.out.println("Размер массива ages равен: " + ages.length);
-        System.out.println("Размер массива surnames равен: " + surnames.length);
-        System.out.println("Размер массива price равен: " + price.length);
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
     }
 }
 
